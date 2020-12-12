8a257facb2493ea1047dfa92a7cbb316bc3b13a5
wielandalex <wielandalex@users.noreply.github.com>
2020-12-12 07:38:09 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.e413062e2645dc2e2ba303cee357a30f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b0bc29fb1603b2421cf",
+    "url": "app.1902138100aec4528cae.bundle.js"
+  },
+  {
+    "revision": "80ce87e2985a50a0e2f3",
+    "url": "runtime.58908f3dbdb804a00215.bundle.js"
+  },
+  {
+    "revision": "6b5954b1c3182a4a63f7",
+    "url": "vendors.c2e7ff05f0f6ad87c022.bundle.js"
+  },
+  {
+    "revision": "3b2b60803bee25ed494f716699c115b0",
+    "url": "index.html"
+  }
+];
